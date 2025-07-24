d22b63b513ab31b1e05f43adb440c31dbddde9e3
Mikhail Durmanov <madurmanov@gmail.com>
2025-07-25 02:56:23 +0300

    Update svg sprites

--- webpack.icons.js
+++ webpack.icons.js
@@ -2,6 +2,22 @@ const path = require('path');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 const RemovePlugin = require('remove-files-webpack-plugin');
 
+const svgSpriteLoader = {
+  loader: 'svg-sprite-loader',
+  options: {
+    extract: true,
+    spriteFilename: 'sprite.svg',
+    symbolId: 'icon-[name]',
+  },
+};
+
+const svgoLoaderCommonPlugin = {
+  name: 'removeAttrs',
+  params: {
+    attrs: '(fill|stroke|fill-opacity|stroke-opacity|opacity)',
+  },
+};
+
 module.exports = {
   mode: 'production',
   entry: path.resolve(__dirname, 'src/icons/index.js'),
@@ -13,30 +29,44 @@ module.exports = {
     rules: [
       {
         test: /\.svg$/,
-        include: path.resolve(__dirname, 'src/icons'),
+        include: path.resolve(__dirname, 'src/icons/color'),
+        use: [svgSpriteLoader],
+      },
+      {
+        test: /\.svg$/,
+        include: path.resolve(__dirname, 'src/icons/fill'),
         use: [
-          {
-            loader: 'svg-sprite-loader',
-            options: {
-              extract: true,
-              spriteFilename: 'sprite.svg',
-              symbolId: 'icon-[name]',
-            },
-          },
+          svgSpriteLoader,
           {
             loader: 'svgo-loader',
             options: {
               plugins: [
-                {
-                  name: 'removeAttrs',
-                  params: {
-                    attrs: '(fill|stroke|fill-opacity)',
-                  },
-                },
+                svgoLoaderCommonPlugin,
                 {
                   name: 'addAttributesToSVGElement',
                   params: {
-                    attributes: [{ fill: 'currentColor' }],
+                    attributes: [{ fill: 'currentColor', stroke: 'none' }],
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        include: path.resolve(__dirname, 'src/icons/stroke'),
+        use: [
+          svgSpriteLoader,
+          {
+            loader: 'svgo-loader',
+            options: {
+              plugins: [
+                svgoLoaderCommonPlugin,
+                {
+                  name: 'addAttributesToSVGElement',
+                  params: {
+                    attributes: [{ stroke: 'currentColor', fill: 'none' }],
                   },
                 },
               ],
